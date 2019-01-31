48916fce4457dc7c795148c7cfc21bc283235e8b
Reilly Bova <rbova@princeton.edu>
2019-01-31 15:13:45 +0100

    better nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -10,17 +10,32 @@ import { Link } from 'gatsby';
 import { PrincetonShield } from 'images';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  appBar: {
-    borderStyle: "solid",
-    borderWidth: "0px 0px 4px 0px",
-    borderColor: theme.palette.primary.main
-  }
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    appBar: {
+        borderStyle: "solid",
+        borderWidth: "0px 0px 4px 0px",
+        borderColor: theme.palette.primary.main
+    },
+    navButton: {
+        borderRadius: 0,
+        fontSize: 16,
+        [theme.breakpoints.down('xs')]: {
+          '@media (orientation: landscape)': {
+            height: 48,
+          },
+          '@media (orientation: portrait)': {
+            height: 56,
+          }
+        },
+        [theme.breakpoints.up('sm')]: {
+            height: 64,
+        }
+    }
 });
 
 function Header(props) {
@@ -30,7 +45,7 @@ function Header(props) {
             <AppBar position="fixed" className={classes.appBar} color="secondary">
                 <Toolbar>
                 <Link to="/">
-                    <img src={PrincetonShield} style={{margin: "6px", height: "42px"}}/>
+                    <img src={PrincetonShield} style={{margin: "3px", height: "42px"}}/>
                 </Link>
                     <Typography variant="h4" color="inherit" className={classes.grow}>
                         <Link
@@ -38,16 +53,27 @@ function Header(props) {
                             style={{
                                 color: 'white',
                                 textDecoration: 'none',
+                                maxHeight: 48,
+                                overflow: "hidden",
+                                whiteSpace: "nowrap"
                             }}
                             >
                             <span style={{ fontWeight: '800' }}>{"COS"}</span>
                             <span style={{ fontWeight: '400' }}>{" 426"}</span>
                         </Link>
                     </Typography>
-                    <Button color="inherit">Syllabus</Button>
-                    <Button color="inherit">Materials</Button>
-                    <Button color="inherit">Assignments</Button>
-                    <Button color="inherit">Gallery</Button>
+                    <Button size="large" classes={{root: classes.navButton}} color="primary">
+                        Syllabus
+                    </Button>
+                    <Button size="large" classes={{root: classes.navButton}} color="inherit">
+                        Materials
+                    </Button>
+                    <Button size="large" classes={{root: classes.navButton}} color="inherit">
+                        Assignments
+                    </Button>
+                    <Button size="large" classes={{root: classes.navButton}} color="inherit">
+                        Gallery
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
