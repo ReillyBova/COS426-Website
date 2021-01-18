f9e8320834dea5aa77f15033aa94465fa345e03c
Reilly Bova <reillybova@gmail.com>
2021-01-17 18:49:28 -0800

    Added april fools mode

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -2,11 +2,18 @@ import React, { Fragment, useLayoutEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ToggleThemeContext, light_theme, dark_theme } from 'theming';
-import { setLocalStorage, getLocalStorage } from 'utils';
+import {
+    ToggleThemeContext,
+    light_theme,
+    dark_theme,
+    harvard_theme,
+    nassauOrange_theme,
+} from 'theming';
+import { setLocalStorage, getLocalStorage, isAprilFoolsDay } from 'utils';
 
 export default function TopLayout(props) {
     const [isLightTheme, setIsLightTheme] = useState(true);
+    const [isAprilFools, setAprilFools] = useState(false);
 
     const toggleTheme = () => {
         const newSetting = !isLightTheme;
@@ -14,7 +21,12 @@ export default function TopLayout(props) {
         setLocalStorage('selected-theme', newSetting, 12);
     };
 
-    const activeTheme = isLightTheme ? light_theme : dark_theme;
+    let activeTheme;
+    if (isAprilFools) {
+        activeTheme = isLightTheme ? harvard_theme : nassauOrange_theme;
+    } else {
+        activeTheme = isLightTheme ? light_theme : dark_theme;
+    }
 
     useLayoutEffect(() => {
         // Check for user preference is saved
@@ -26,10 +38,16 @@ export default function TopLayout(props) {
             }
         } else {
             // Set to dark mode from 8am to 8pm
-            const currentHour = new Date().getHours();
+            const now = new Date();
+            const currentHour = now.getHours();
             if (currentHour < 8 || currentHour > 20) {
                 setIsLightTheme(false);
             }
+        }
+
+        // Check if it is april fools :P
+        if (isAprilFoolsDay()) {
+            setAprilFools(true);
         }
     }, []);
 
--- src/theming/theme.js
+++ src/theming/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-// Princeton Theme
+// Princeton Light Theme
 export const light_theme = responsiveFontSizes(
     createMuiTheme({
         palette: {
@@ -28,7 +28,7 @@ export const light_theme = responsiveFontSizes(
     })
 );
 
-// Princeton Theme
+// Princeton Dark Theme
 export const dark_theme = responsiveFontSizes(
     createMuiTheme({
         palette: {
@@ -42,6 +42,63 @@ export const dark_theme = responsiveFontSizes(
             background: {
                 default: '#292527',
                 paper: '#312f31',
+            },
+            contrastThreshold: 2,
+        },
+        typography: {
+            fontFamily: 'Open Sans, Helvetica, Arial, sans-serif',
+            fontWeightLight: 400,
+            fontWeightRegular: 600,
+            fontWeightMedium: 700,
+            body1: {
+                fontWeight: 400,
+            },
+        },
+    })
+);
+
+// Harvard Theme (April Fools)
+export const harvard_theme = responsiveFontSizes(
+    createMuiTheme({
+        palette: {
+            primary: {
+                main: '#A51C30',
+            },
+            secondary: {
+                main: '#293352',
+            },
+            background: {
+                default: '#f2f2f2',
+                paper: '#fff',
+            },
+            contrastThreshold: 2,
+        },
+        typography: {
+            fontFamily: 'Open Sans, Helvetica, Arial, sans-serif',
+            fontWeightLight: 400,
+            fontWeightRegular: 600,
+            fontWeightMedium: 700,
+            body1: {
+                fontWeight: 400,
+            },
+        },
+    })
+);
+
+// Hause of Nassau-Orange Based Theme
+export const nassauOrange_theme = responsiveFontSizes(
+    createMuiTheme({
+        palette: {
+            type: 'dark',
+            primary: {
+                main: '#f58025',
+            },
+            secondary: {
+                main: '#1038b6',
+            },
+            background: {
+                default: '#2b4b4d',
+                paper: '#3da5a6',
             },
             contrastThreshold: 2,
         },
