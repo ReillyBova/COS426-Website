06e6a50b084fd9b5b1dc0a7dd9bb14f4710d9e5d
EthanTseng123 <ethan.tseng@yahoo.com>
2021-06-19 21:31:03 -0400

    Re-organize gifs.

--- src/assets/gifs/projects/index.js
+++ src/assets/gifs/projects/index.js
@@ -1,8 +1,17 @@
-export { default as CoreCrawler } from './CoreCrawler.gif';
-export { default as DJSimulator } from './DJSimulator.gif';
-export { default as GetDown } from './GetDown.gif';
-export { default as Portal_Half } from './Portal_Half.gif';
-export { default as SailingSim } from './SailingSim.gif';
-export { default as TheCakeryBakery } from './TheCakeryBakery.gif';
-export { default as VSKeys } from './VSKeys.gif';
-export { default as PsychedelicSimulator } from './PsychedelicSimulator.gif';
+export { default as CoreCrawler } from './2021/CoreCrawler.gif';
+export { default as DJSimulator } from './2021/DJSimulator.gif';
+export { default as GetDown } from './2021/GetDown.gif';
+export { default as Portal_Half } from './2021/Portal_Half.gif';
+export { default as SailingSim } from './2021/SailingSim.gif';
+export { default as TheCakeryBakery } from './2021/TheCakeryBakery.gif';
+export { default as VSKeys } from './2021/VSKeys.gif';
+export { default as PsychedelicSimulator } from './2021/PsychedelicSimulator.gif';
+
+export { default as cat_dash } from './2020/cat_dash.gif';
+export { default as dream_world } from './2020/dream_world.gif';
+export { default as drivers_ed } from './2020/drivers_ed.gif';
+
+export { default as collideoscope } from './2019/collideoscope.gif';
+export { default as colo_ring } from './2019/colo_ring.gif';
+export { default as death_star_assault } from './2019/death_star_assault.gif';
+export { default as shape_battles } from './2019/shape_battles.gif';
