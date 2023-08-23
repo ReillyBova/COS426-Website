e29b9a01a8e8c41d8dba8a7b9bb7bba0503c87d2
Reilly Bova <reillybova@gmail.com>
2023-08-23 07:18:30 -0700

    Fix

--- src/Pages/Overview/Staff.tsx
+++ src/Pages/Overview/Staff.tsx
@@ -1,59 +1,59 @@
-// Library imports
-import React, { useState } from 'react';
+// // Library imports
+// import React, { useState } from 'react';
 
-import Button from '@material-ui/core/Button';
-import Slide from '@material-ui/core/Slide';
-// UI imports
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import Slide from '@material-ui/core/Slide';
+// // UI imports
+// import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
 
-// Local imports
-import { StaffGrid } from './subcomponents';
+// // Local imports
+// import { StaffGrid } from './subcomponents';
 
-// Custom styling for component
-const staffStyles = makeStyles((theme) => ({
-    staff_desktop: {
-        [theme.breakpoints.down('md')]: {
-            display: 'none',
-        },
-    },
-    staff_mobile: {
-        [theme.breakpoints.up('lg')]: {
-            display: 'none',
-        },
-    },
-    toggle: {
-        width: '100px',
-    },
-}));
+// // Custom styling for component
+// const staffStyles = makeStyles((theme) => ({
+//     staff_desktop: {
+//         [theme.breakpoints.down('md')]: {
+//             display: 'none',
+//         },
+//     },
+//     staff_mobile: {
+//         [theme.breakpoints.up('lg')]: {
+//             display: 'none',
+//         },
+//     },
+//     toggle: {
+//         width: '100px',
+//     },
+// }));
 
-function Staff() {
-    // State to determine if staff should be displayed
-    const [isVisible, setVisibility] = useState(false);
-    const toggleVisibility = () => {
-        setVisibility(!isVisible);
-    };
+// function Staff() {
+//     // State to determine if staff should be displayed
+//     const [isVisible, setVisibility] = useState(false);
+//     const toggleVisibility = () => {
+//         setVisibility(!isVisible);
+//     };
 
-    // CSS classes for styling
-    const classes = staffStyles();
+//     // CSS classes for styling
+//     const classes = staffStyles();
 
-    return (
-        <Typography component='div' paragraph>
-            <div className={classes.staff_desktop}>
-                <StaffGrid />
-            </div>
-            <div className={classes.staff_mobile}>
-                <Typography component={'div'} paragraph align='center'>
-                    <Button variant='contained' color='primary' className={classes.toggle} onClick={toggleVisibility}>
-                        {isVisible ? 'Hide' : 'Show'}
-                    </Button>
-                </Typography>
-                <Slide direction='up' in={isVisible} mountOnEnter unmountOnExit>
-                    <StaffGrid />
-                </Slide>
-            </div>
-        </Typography>
-    );
-}
+//     return (
+//         <Typography component='div' paragraph>
+//             <div className={classes.staff_desktop}>
+//                 <StaffGrid />
+//             </div>
+//             <div className={classes.staff_mobile}>
+//                 <Typography component={'div'} paragraph align='center'>
+//                     <Button variant='contained' color='primary' className={classes.toggle} onClick={toggleVisibility}>
+//                         {isVisible ? 'Hide' : 'Show'}
+//                     </Button>
+//                 </Typography>
+//                 <Slide direction='up' in={isVisible} mountOnEnter unmountOnExit>
+//                     <StaffGrid />
+//                 </Slide>
+//             </div>
+//         </Typography>
+//     );
+// }
 
-export default Staff;
+// export default Staff;
